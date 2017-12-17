6665c0df9bcfb2b9c2f5566211857dfbb316725d
toyoharu nishikawa <toyohal@email.com>
2017-12-17 18:27:51 +0900

    add node js and mongo db

---
+++ public/wachable/scripts/main.js
@@ -0,0 +1,69 @@
+var drawElement = document.getElementById("draw");
+var tabElements = document.querySelectorAll(".maintab > li");
+var mainElements = document.querySelectorAll(".main > li");
+
+var req = new XMLHttpRequest();
+
+var control = {
+  pushButton: function(){
+      let self = this;
+      document.getElementById('button').addEventListener('click',self.send,false);
+  },
+  send: function(jsonText){
+    req.open("POST", "data_registry",true);
+    req.addEventListener('load',function(e){
+      console.log("save data successfully");
+      document.removeEventListener('load',arguments.callee,false)
+    },false);
+    req.setRequestHeader("content-type","application/json");
+    console.log(jsonText);
+    req.responseType = "json";
+    req.send(jsonText);
+    return this
+  },
+}
+
+
+drawElement.addEventListener('click',function(){
+  let editorData = editor.getValue();
+  let plotData = JSON.parse(editorData);
+  control.send(editorData);
+  console.log(plotData.data)
+  Plotly.newPlot('plotly-div',
+    plotData.data,
+    plotData.layout,
+    {
+      editable: true,
+      showLink: false,
+      displaylogo: false,
+      modeBarButtonsToRemove: ['sendDataToCloud']
+    }
+  );
+  let draw = document.createEvent("HTMLEvents");
+  draw.initEvent("click", true, false);
+  tabElements[1].dispatchEvent(draw);
+},false)
+
+tabElements.forEach(function(value,index,array){
+  value.addEventListener("click",function(e){
+    e.preventDefault();
+    let target = e.target;
+    console.log(array)
+    array.forEach(function(value2,index2,array2){
+      value2.className=null;
+    });
+    target.className = "select";
+    mainElements.forEach(function(value2,index3,array2){
+      value2.className=null;
+    })
+    var p = e.target.parentElement;
+    
+    //get index of clicked element
+    var index = Array.prototype.indexOf.call(p.children, e.target);
+    console.log(index)
+    mainElements[index].className="select";
+  },false);
+});
+
+
+
